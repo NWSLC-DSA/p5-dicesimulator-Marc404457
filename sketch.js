b77d29775ec9187501e70cfb6561c72bda82c59c
Marc404457 <104359832+Marc404457@users.noreply.github.com>
2022-05-06 00:57:56 +0100

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -38,7 +38,7 @@ const computer = {
 };
 
 function setup() {
-  player.posX = width / 2 - dice.size * 0.25;
+  player.name = player.posX = width / 2 - dice.size * 0.25;
   player.posY = height / 2;
   computer.posX = width / 2 + dice.size * 1.25;
   computer.posY = height / 2;
@@ -57,7 +57,7 @@ function draw() {
     drawDie(computer);
     drawFace(player.roll, player);
     drawFace(computer.roll, computer);
-    console.log("Click...");
+    //console.log("clicked");
   }
   dice.isPressed = false;
 }
@@ -67,8 +67,13 @@ function buttonPressed() {
   player.roll = rollDice();
   computer.roll = rollDice();
 
-  console.log(`You rolled a...\n${player.roll}`);
-  console.log(`The computer rolled a...\n${computer.roll}`);
+  let winner = whoWon(player.roll, computer.roll);
+
+  console.log(`
+    You rolled a, ${player.roll}
+    The computer rolled a, ${computer.roll}
+
+    The winner is ${winner}`);
 }
 
 function drawDie(owner) {
@@ -97,9 +102,9 @@ function drawFace(rollValue, owner) {
     [owner.posX - dice.size * 0.25, owner.posY], // Middle Right, index 6 PINK
   ];
 
-  console.log(`${owner.name} posX is ${owner.posX}, posY is ${owner.posY}`);
+  //console.log(`${owner.name} posX is ${owner.posX}, posY is ${owner.posY}\n`);
   let randomStroke = Math.floor(Math.random() * 16777215).toString(16);
-  console.log(`${owner.name}'s colour is ${randomStroke}`);
+  //console.log(`${owner.name}'s colour is ${randomStroke}`);
 
   let rgb = parseInt(randomStroke, 16); // convert rrggbb to decimal
   let redScale = (rgb >> 16) & 0xff; // extract red
@@ -108,10 +113,12 @@ function drawFace(rollValue, owner) {
 
   let luma = 0.2126 * redScale + 0.7152 * greenScale + 0.0722 * blueScale;
 
-  if (luma < 40 || luma > 200) {
+  //console.log(`Luma = ${redScale} ${greenScale} ${blueScale} = ${luma}`);
+  if (luma < 100 || luma > 200) {
     stroke(`#${randomStroke}`);
-    console.log(`Changed colour to ${randomStroke}`);
+    //console.log(`Changed ${owner.name} colour to ${randomStroke}`);
   }
+
   strokeWeight(_strokeWeight);
   if (rollValue == 1) {
     //stroke(`red`);
@@ -165,6 +172,22 @@ function drawFace(rollValue, owner) {
     console.log("ICH BIN KAPUT!!");
   }
   strokeWeight(1);
+}
+
+function whoWon(playerRoll, computerRoll) {
+  if (playerRoll > computerRoll) {
+    player.won += 1;
+    computer.lost += 1;
+    return player.name;
+  } else if (playerRoll < computerRoll) {
+    computer.won += 1;
+    player.lost += 1;
+    return computer.name;
+  } else {
+    player.draw += 1;
+    computer.draw += 1;
+    return "no one";
+  }
 }
 
 function rollDice() {
